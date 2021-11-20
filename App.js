00d53e44249865431d31c9a9e01e9cbd5ed8e3a2
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-11-21 08:57:03 +0900

    Try to get response from Yahoo for test

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+  // state를 선언하고, 날씨 정보를 가져와서, 화면에 뿌리기
+  // const { weather, setWeather } = useState([]);
+
+  useEffect(async () => {
+    const client = axios.create({
+      baseURL: "https://www.yahoo.co.jp/",
+    });
+    const data = await client.get("");
+    console.log(data);
+    // setWeather(data);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <Text>Below is weather</Text>
+      {/*<Text>{weather}</Text>*/}
       <StatusBar style="auto" />
     </View>
   );
