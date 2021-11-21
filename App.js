c14efc1adf09d45b38aec5dac112b40295fba975
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-11-21 21:20:59 +0900

    Try to set text

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import config from "./config";
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import axios from 'axios';
 
 const API_KEY = config.API_KEY;
@@ -10,6 +10,12 @@ export default function App() {
 
   // state를 선언하고, 날씨 정보를 가져와서, 화면에 뿌리기
   const [ weather, setWeather ] = useState([]);
+  const [ sentences, setSentences ] = useState("hi");
+
+  inputText = () => {
+    const text = document.TextInput.value;
+    setSentences(text);
+  };
 
   useEffect(async () => {
     const client = axios.create({
@@ -25,6 +31,17 @@ export default function App() {
     <View style={styles.container}>
       <Text>Below is weather</Text>
       <Text>{weather}</Text>
+
+      <Text>{sentences}</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Please input sentences"
+      />
+      <Button
+        onPress={() => inputText}
+        title="Save"
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -36,5 +53,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
